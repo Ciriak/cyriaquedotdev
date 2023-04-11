2cea0c258b5e9d3f960d5184264df77a3a60121d
Cyriaque <cyriaquedelaunay@gmail.com>
2023-04-11 21:37:30 +0200

    rollback and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Work, { IWorkProps } from './components/Work';
 import './app.scss';
 
 interface link {
@@ -54,11 +53,6 @@ const links: link[] = [
     label: 'My Steam account',
   },
   {
-    url: 'https://www.reddit.com/user/CYRIAQU3/',
-    icon: 'reddit.png',
-    label: 'My Reddit account',
-  },
-  {
     url: 'https://twitter.com/Ciriak_',
     icon: 'twitter.png',
     label: 'My Twitter',
@@ -70,73 +64,18 @@ const links: link[] = [
   },
 ];
 
-const works: IWorkProps[] = [
-  {
-    image: 'https://i.imgur.com/aZFLk0Z.gif',
-    title: 'Carnelian',
-    desc: 'A software that allow you to run userscripts on desktop',
-    url: 'http://www.carnelian.io',
-  },
-  {
-    image: '/work/scanner.png',
-    title: 'Steam Scanner',
-    desc:
-      'Steam Scanner is an utility that runs as a background process, grabs your games from others launchers and adds them to your Steam library.',
-    url: 'https://steam-scanner.com',
-  },
-  {
-    image: '/work/youtube-cutter.png',
-    title: 'Youtube Cutter',
-    desc: 'An online tool that allow you to cut and download any Youtube clip as a video or audio file',
-    url: 'https://youtube-cutter.tools',
-  },
-  {
-    image: '/work/tagifier.jpg',
-    title: 'Tagifier',
-    desc: 'A metadata editor for mp3 files.<br>With Tagifier you can add some metadata like the composer of the song or a custom cover image',
-    url: 'https://github.com/Ciriak/Tagifier',
-  },
-  {
-    image: '/work/dailywallpaper.png',
-    title: 'Daily Wallpaper',
-    desc: 'A Twitter bot who post a new Wallpaper every day',
-    url: 'https://twitter.com/DLWallpaperBot',
-  },
-  {
-    image: '/work/findpaper.jpg',
-    title: 'Findpaper',
-    desc: 'A small Wallpaper search engine',
-    url: 'http://findpaper.tech',
-  },
-  {
-    image: '/work/codepen.png',
-    title: 'My Codepen',
-    desc: 'A collection of small web experiments.',
-    url: 'https://codepen.io/Ciriak/pens/showcase',
-  },
-  {
-    image: '/work/ludum.png',
-    title: 'Ludum Dare entries',
-    desc:
-      'My entries for the <b>Ludum Dare</b>,<br> a competition where you have to create a game in 48 hours.<br>The compos were done in solo and the jams with some friends',
-    url: 'https://ldjam.com/users/cyriaque/games',
-  },
-];
-
 function App() {
   const originalSubTitle = 'Front-End developer';
 
   const [subtitle, setSubtitle] = useState(originalSubTitle);
   const [videoUrl, setVideoUrl] = useState('');
   const [bgLabel, setBgLabel] = useState('');
-  const [showBgLabel, setShowBgLabel] = useState(true);
-  const [showAnchor, setShowAnchor] = useState(true);
+  const [showBgLabel] = useState(true);
   const [videoLoaded, setVideoLoaded] = useState(false);
-  const [videoBgOffset, setVideoBgOffset] = useState(0);
+  const [videoBgOffset] = useState(0);
 
   useEffect(() => {
     setVideoBg();
-    listenScroll();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -176,13 +115,6 @@ function App() {
             })}
           </div>
 
-          <a className={`work-anchor ${showAnchor ? 'active' : ''}`} href="#work">
-            <span>MY WORK</span>
-            <div className="chevron">
-              <img src="/chevron-down.png" alt="My work" />
-            </div>
-          </a>
-
           <div className={`bg-infos ${showBgLabel ? 'active' : ''}`}>
             <span className="background-label">
               {bgLabel}    
@@ -194,18 +126,6 @@ function App() {
                 [change]
               </b>
             </span>
-          </div>
-        </div>
-      </div>
-
-      <div className="section work-section" id="work">
-        <div className="section-bg"></div>
-        <div className="section-content">
-          <h1>My work</h1>
-          <div className="works-container">
-            {works.map((work: IWorkProps, workIndex: number) => {
-              return <Work key={workIndex} desc={work.desc} title={work.title} url={work.url} image={work.image} />;
-            })}
           </div>
         </div>
       </div>
@@ -229,17 +149,6 @@ function App() {
     currentBg = background;
     setVideoUrl('/video-bg/' + background.name + '.mp4');
     setBgLabel(background.label);
-  }
-
-  /**
-   * Listen for user scroll
-   */
-  function listenScroll() {
-    window.onscroll = function (e: any) {
-      setShowBgLabel(window.scrollY === 0);
-      setShowAnchor(window.scrollY < this.window.innerHeight / 3);
-      setVideoBgOffset(window.scrollY / 1.5);
-    };
   }
 }
 
