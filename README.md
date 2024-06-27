402f2e1db42f0be2bc08ee821aee5b345dd5ae2f
Cyriaque <cyriaquedelaunay@gmail.com>
2024-06-27 19:27:13 +0200

    feat: updated project to vuejs

--- README.md
+++ README.md
@@ -1,44 +1,45 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# cyriaquedotdev
 
-## Available Scripts
+This template should help get you started developing with Vue 3 in Vite.
 
-In the project directory, you can run:
+## Recommended IDE Setup
 
-### `npm start`
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Type Support for `.vue` Imports in TS
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.
 
-### `npm test`
+## Customize configuration
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+See [Vite Configuration Reference](https://vitejs.dev/config/).
 
-### `npm run build`
+## Project Setup
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+```sh
+npm install
+```
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+### Compile and Hot-Reload for Development
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+```sh
+npm run dev
+```
 
-### `npm run eject`
+### Type-Check, Compile and Minify for Production
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+```sh
+npm run build
+```
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+### Run Unit Tests with [Vitest](https://vitest.dev/)
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+```sh
+npm run test:unit
+```
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+### Lint with [ESLint](https://eslint.org/)
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+```sh
+npm run lint
+```
---
+++ src/views/HomeView.vue
@@ -0,0 +1,147 @@
+<template>
+  <main>
+    <div class="cyriaque">
+      <div class="section main-section">
+        <div
+          :class="{
+            'section-bg': true,
+            'video-bg': true,
+            loaded: videoLoaded
+          }"
+        >
+          <video :src="String(videoUrl)" autoplay loop muted @canplay="videoLoaded = true"></video>
+        </div>
+        <div class="section-content">
+          <header>
+            <h1>Cyriaque Delaunay</h1>
+            <h2>{{ subtitle }}</h2>
+          </header>
+          <div class="links-container">
+            <a
+              v-for="(link, linkIndex) in links"
+              :href="link.url"
+              :key="linkIndex"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div
+                class="link"
+                @mouseenter="subtitle = link.label"
+                @mouseleave="subtitle = originalSubTitle"
+              >
+                <img :src="link.icon" :alt="link.label" />
+              </div>
+            </a>
+          </div>
+
+          <div :class="`bg-infos ${showBgLabel ? 'active' : ''}`">
+            <span class="background-label">
+              {{ bgLabel }} &nbsp;&nbsp;
+              <b @click="updateVideoBackground()">[change]</b>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+interface link {
+  label: string
+  url: string
+  icon: string
+}
+
+interface Background {
+  name: string
+  label: string
+}
+
+let currentBg = ref()
+
+const backgrounds: Background[] = [
+  {
+    name: 'tf2',
+    label: 'Team Fortress 2 (2007)'
+  },
+  {
+    name: 'halo',
+    label: 'Halo : The Master Chief Collection (2014)'
+  },
+  {
+    name: 'dkc2',
+    label: 'Donkey Kong Country 2 (1995)'
+  },
+  {
+    name: 'skyrim',
+    label: 'The Elder Scrolls V : Skyrim (2011)'
+  },
+  {
+    name: 'ori',
+    label: 'Ori and the blind forest (2015)'
+  },
+  {
+    name: 'celeste',
+    label: 'Celeste (2018)'
+  }
+]
+
+const links: link[] = [
+  {
+    url: 'https://github.com/Ciriak',
+    icon: 'github.png',
+    label: 'My Github'
+  },
+  // {
+  //   url: 'http://steamcommunity.com/id/Ciriak',
+  //   icon: 'steam.png',
+  //   label: 'My Steam account'
+  // },
+  {
+    url: 'https://x.com/Ciriak_',
+    icon: 'x.png',
+    label: 'My X account'
+  },
+  {
+    url: 'https://www.linkedin.com/in/cyriaque-delaunay-833504121/',
+    icon: 'linkedin.png',
+    label: 'My LinkedIn'
+  }
+]
+
+const originalSubTitle = 'Front-End developer'
+
+const subtitle = ref(originalSubTitle)
+const videoUrl = ref('')
+const bgLabel = ref('')
+const showBgLabel = ref(true)
+const videoLoaded = ref(false)
+
+/**
+ * Select a random video and return its url
+ */
+function getVideoBg() {
+  const background = backgrounds[Math.floor(Math.random() * backgrounds.length)]
+  return background
+}
+
+/**
+ * Update the video background
+ */
+function updateVideoBackground() {
+  const background = getVideoBg()
+  if (background === currentBg.value) {
+    updateVideoBackground()
+    return
+  }
+  currentBg.value = background
+  videoUrl.value = '/video-bg/' + background.name + '.mp4'
+  bgLabel.value = background.label
+}
+
+onMounted(() => {
+  updateVideoBackground()
+})
+</script>
